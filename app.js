75d42eb899e9f8a86d8db5f3c24f7a3e6cf3f2a1
MiareJ <psdsd@naver.com>
2021-09-09 03:04:30 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+//jshint esversion:6
+const express = require("express");
+
+const app = new express();
+
+let items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  const today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  const day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+
+  items.push(item);
+  
+  res.redirect("/");
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
