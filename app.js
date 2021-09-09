3d51ec1438f5dd3f7374bfb098db5e49c7f8479d
MiareJ <psdsd@naver.com>
2021-09-10 01:50:26 +0900

    complete EJS section

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 //jshint esversion:6
 const express = require("express");
+const date = require(__dirname + "/date.js");
 
 const app = new express();
 
-let items = [];
+const items = ["Studying Programming"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  const today = new Date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
+  let day = date.getDate();
 
-  const day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res) {
-  const item = req.body.newItem;
-
-  items.push(item);
   
-  res.redirect("/");
+  let item = req.body.newItem;
 
+  if(req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function() {
