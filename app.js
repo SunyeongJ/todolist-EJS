5923f5acbed3597a10bccde25e8a07dddb1b83b3
MiareJ <psdsd@naver.com>
2021-09-19 00:42:04 +0900

    add customList and delete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
-const e = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = new express();
 
@@ -62,30 +62,48 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
   
   const itemName = req.body.newItem;
+  const listName = req.body.list;
+
   const item = new Item ({
     name: itemName
   });
 
-  item.save();
-  res.redirect("/");
+  if(listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+  
 });
 
 app.post("/delete", (req, res) => {
   const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
 
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if(!err){
-      console.log("Successfully deleted");
-      res.redirect("/");
-    } else {
-      console.log(err);
-    }
-  });
+  if(listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if(!err){
+        console.log("Successfully deleted");
+        res.redirect("/");
+      } 
+    })
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList) {
+      if(!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
   
 });
 
 app.get("/:customListName", function(req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
   
   List.findOne({name: customListName}, function(err, foundList) {
     if(!err) {
